b1cc2cb6239644dc188c57024ff3011d38b0d00f
Arialdo Martini <arialdomartini@gmail.com>
2013-09-20 23:34:08 +0200

    interpreta un file dat e crea il rispettivo Context

---
+++ teams.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import sys
+import getopt
+import re
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class Context():
+
+    def load_from_file(self, file_path):
+        with open(file_path) as f:
+            lines = f.readlines() 
+            if len(lines) != 2:
+                raise Exception("bad file %s" % file_path)
+            else:
+                name = re.match("\[TEAM\]=\"(.*)\"", lines[0]).groups()[0]
+                countries_string = re.match("\[COUNTRIES\]=\"(.*)\"", lines[1]).groups()[0]
+                countries = countries_string.split(" ")
+
+                self.name = name
+                self.countries = countries
+
+
+
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        except getopt.error, msg:
+             raise Usage(msg)
+
+
+        context = Context()
+        context.load_from_file("./dwh.dat")
+        print context.name
+        print context.countries
+
+
+
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "for help use --help"
+        return 2
+
+if __name__ == "__main__":
+    sys.exit(main())
